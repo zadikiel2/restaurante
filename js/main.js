ebba1d37db0255fe99325d1ae98073941d16e3e0
Valencia <fernando.valencia140471@gmail.com>
2020-04-17 00:31:44 -0700

    v4

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//manda mensaje //var contra = prompt("ingresa contrasena");
+//console.log(contra);
+
+
+function changeColor(newColor){
+    var elem = document.getElementById('elemento');
+     elem.style.color= newColor;
+
+}
